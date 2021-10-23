5ff24b41151b8fbb81e652c5a8e9a891a7317f27
Md Adil Alam <31787784+Adilalaminfo@users.noreply.github.com>
2021-10-24 04:54:16 +0530

    Add files via upload

---
+++ ArrayofObject.js
@@ -0,0 +1,11 @@
+var nam=[
+    {
+        firstName:'Adil',
+        lastName:'Alam'
+    },
+    {
+        Age:23,
+        RollN:21
+    }
+];
+console.log(nam[2].Age);
---
+++ Javascript object.js
@@ -0,0 +1,9 @@
+var name={
+    firstName:'Md Adil',
+    lastName:'Alam',
+    age: 23
+},
+fullName: function(){
+    return this.firstName+" "+this.lastName;
+}
+console.log(name.fullName()); 
